71d269c9e6716c156f517c76d7e2497753f2e65e
rexxarpan <bezborodov.roma@list.ru>
2020-04-05 18:07:18 +0700

    Добавьте файлы проекта.

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+	//setlocale(0, "");
+	//int a, b;
+	//cout << "10: ";
+	//cin >> a;
+	//cout << "4: ";
+	//cin >> b;
+	//int c = a - b;
+	//cout << "Разность чисел = " << c << endl;
+/*
+	int a{ 10 };
+	int b{ 5 };
+
+	std::cout << a - b << std::endl;
+
+	std::string name;
+	std::cin >> name;
+	std::cout << name << std::endl;
+*/
+
+	float fp{ 0.f };
+
+	std::cin >> fp;
+
+	return 0;
+}
